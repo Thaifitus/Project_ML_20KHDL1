45f2420967a167db8e23e9beadc8552e13d42a2a
Thaifitus <nththai20@clc.fitus.edu.vn>
2023-05-05 00:32:44 +0700

    Change app.py to streamlit (was html for front-end)

--- app.py
+++ app.py
@@ -1,24 +1,21 @@
-from flask import Flask, request, render_template
-from flask_cors import CORS
+# from flask import Flask, request
+# from flask_cors import CORS
 import numpy as np
 import torch
 from pytorch_transformers import BertTokenizer, BertForMaskedLM
 import nltk
+import streamlit as st
 
-app = Flask(__name__)
-CORS(app)
+# app = Flask(__name__)
+# CORS(app)
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertForMaskedLM.from_pretrained('bert-base-uncased', output_attentions=True)
 model.eval()
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route('/fillblanks', methods=['POST'])
+# @app.route('/fillblanks', methods=['POST'])
 def predict():
-	sentence_orig = request.form.get('text')
+	sentence_orig = st.text_input('Input text:', 'I ____ you')
 	if '____' not in sentence_orig:
 		return sentence_orig
 
@@ -61,9 +58,17 @@ def predict():
 	# 	print ()
 	predicted_index = torch.argmax(predictions[0, masked_index]).item()
 	predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])[0]
-	for f in focus:
-		sentence_orig = sentence_orig.replace(f, '<font color="blue">'+f+'</font>')
-	return sentence_orig.replace('____', '<font color="red"><b><i>'+predicted_token+'</i></b></font>')
+	# for f in focus:
+	# 	sentence_orig = sentence_orig.replace(f, '<font color="blue">'+f+'</font>')
+	return sentence_orig.replace('____', predicted_token)
 
 if __name__=='__main__':
-	app.run(debug=False)
+	# app.run(debug=False)
+	st.header(":blue[DistillBERT]")
+	st.write(":blue[DistillBERT] is a fill-in-the-blanks model that is trained to predict the missing word in the sentence. For the purpose of this demo we will be using pre-trained distillbert-base-uncased as our prediction model.")
+
+	predicted_sen = predict()
+	st.write(predicted_sen)
+
+	st.write("**Demo Manual**")
+	st.write("1. Use ____ as the marker for representing blank space in the text.\n2. Give only one blank space (____) at a time. More than one blanks are not taken care.")
